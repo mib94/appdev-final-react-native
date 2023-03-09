1bd556bd83013abfe7b09e443b8e3793a0604f19
mib94 <mibnobachir@chicagobooth.edu>
2023-03-09 13:49:13 +0100

    navigation setup

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from "expo-status-bar";
-import SafeArea from "./SafeArea";
-import Navigation from "./Navigation";
+import SafeArea from "./src/SafeArea";
+import Navigation from "./src/Navigation";
 import AuthProvider from "./src/components/AuthProvider";
 
 export default function App() {
